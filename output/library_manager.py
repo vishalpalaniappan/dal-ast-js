237595a45668ead14ed917fc7f930d7bc5b18211
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-07-16 08:41:48 -0400

    Add python program to run synthesized behaviors and connect design to environment.

---
+++ output/library_manager.py
@@ -0,0 +1,35 @@
+from synthesized import *
+
+'''
+    Temporary setup to connect the behaviors to the environment and
+    test the synthesized design. 
+    
+    The text for the menu etc should come from the design but this
+    was just a quick setup to test the synthesis and inspect the
+    world state.
+'''
+
+def main():
+    nextBehavior = "createBasket"
+    worldState = {}
+
+    while True:
+        print("")
+        if nextBehavior == "getChoice":
+            selectedOption_input = input("Menu(a for add book or g for get book): ")
+            output = globals()[nextBehavior](worldState, selectedOption_input)
+        elif nextBehavior == "acceptName":
+            name_input = input("Provide name: ")
+            output = globals()[nextBehavior](worldState, name_input)
+        else:
+            output = globals()[nextBehavior](worldState)
+
+        nextBehavior = output["nextBehavior"]
+        print(output)
+
+        if nextBehavior == None:
+            break
+
+
+if __name__ == "__main__":
+    main()
