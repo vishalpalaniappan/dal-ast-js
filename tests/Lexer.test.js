cfecda7141d5fde2807075fae120f351f2b926ba
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-07-19 00:25:31 -0400

    Expand test to use .dal file and tokensized output to file.

--- tests/Lexer.test.js
+++ tests/Lexer.test.js
@@ -1,16 +1,18 @@
 import {describe, expect, it} from "vitest";
-import { DalLexer
+import {resolve} from "path"
+import {readFile, unlink, writeFile} from "fs/promises"
+import { DalLexer} from "../src/Lexer";
 
- } from "../src/Lexer";
+
 describe("Lexer", () => {
-    it("basic source", () => {
-        const source = `
-        design("le,xer")
-        behavior sample("test") {
-            set("book",["name"],"Harr Potter")
-        }`;
-        const lexer = new DalLexer(source);
+    it("basic source", async () => {
+        const filePath = resolve(__dirname, "./designs/test.dal")
+        const source = await readFile(filePath)
+        const lexer = new DalLexer(source.toString());
 
+        
+        const tokens_output_path = resolve(__dirname, "./output/test_tokens.json")
+        await writeFile(tokens_output_path, JSON.stringify(lexer.scannedTokens));
     });
 
 });
