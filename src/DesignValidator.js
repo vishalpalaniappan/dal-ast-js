5c0b0c2bd33054cf33230c13f1ff69e1603188ba
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-07-19 23:06:14 -0400

    Add file to implement design validator with some comments.

---
+++ src/DesignValidator.js
@@ -0,0 +1,4 @@
+// This will evaluate the semantic world for inconsistencies.
+// Example: Type mistmatch, invalid participants etc. 
+
+// This requires knowledge about the actual commands intended structure, so I will establish that before moving forward with the validation. The synthesis development can happen in parallel because it doesn't impact this process.
