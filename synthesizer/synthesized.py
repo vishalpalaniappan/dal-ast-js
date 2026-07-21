ad82995e95d3fef5a00de530baccc56ef4518336
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-07-20 21:30:31 -0400

    Expand test3 design to add a second behavior to test synthesis.

--- synthesizer/synthesized.py
+++ synthesizer/synthesized.py
@@ -1,5 +1,9 @@
 design = 'lexer'
 
-def behavior(worldState):
+def sample(worldState):
     while isValid:
-        design = 'test'
+        design = 'reusing commands I support'
+
+def sample2(worldState):
+    while notDone:
+        design = 'reusing commands I support'
