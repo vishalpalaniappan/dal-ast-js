df42fe48b15e31c0f72d12ea791f279dcbdff305
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-07-21 14:18:57 -0400

    Move synthesized node generation to separate file.

--- synthesizer/Synthesizer.py
+++ synthesizer/Synthesizer.py
@@ -1,4 +1,5 @@
 import ast
+from getSynthesizedNode import getSynthesizedNode
 
 class Synthesizer:
     
@@ -33,7 +34,7 @@ class Synthesizer:
             Writes the synthesized ast node to the ast tree.
         '''
         self.printTree(indent, dalAstNode["type"])
-        astNodeBody = self.getAstNode(dalAstNode)
+        astNodeBody = getSynthesizedNode(dalAstNode)
         if astNodeBody:
             ast.fix_missing_locations(astNodeBody)
             astOut.body.append(astNodeBody)
@@ -48,65 +49,3 @@ class Synthesizer:
         '''
         spaces = (indent * 4) * " "
         print(f"{spaces}{value}")
-
-    def getAstNode(self, dalAstNode):
-        '''
-            Get the AST node given the dalAST metadata.
-        '''
-        if (dalAstNode["type"] == "behavior"):
-            return ast.FunctionDef(
-                name=dalAstNode["behaviorName"],
-                args=ast.arguments(
-                    posonlyargs=[],
-                    args=[
-                        ast.arg(arg="worldState")
-                    ],
-                    kwonlyargs=[],
-                    kw_defaults=[],
-                    defaults=[],
-                    vararg=None,
-                    kwarg=None
-                ),
-                body=[],
-                decorator_list=[]
-            )
-
-        elif (dalAstNode["type"] == "design"):
-            return ast.Assign(
-                targets=[
-                    ast.Name(id="design", ctx=ast.Store())
-                ],
-                value=ast.Constant(value=dalAstNode["design_name"][0]["value"])
-            )
-        elif (dalAstNode["type"] == "while"):
-            return ast.While(
-                test=ast.Name(id=dalAstNode["args"][0]["value"], ctx=ast.Load()),
-                body=[],
-                orelse=[]
-            )
-        elif (dalAstNode["type"] == "cmd"):
-            if (dalAstNode["command"] == "getInput"):
-                name = dalAstNode["args"][0]["value"]
-                return ast.Assign(
-                    targets=[
-                        ast.Name(id=name, ctx=ast.Store())
-                    ],
-                    value=ast.Call(
-                        func=ast.Name(id="input", ctx=ast.Load()),
-                        args=[],
-                        keywords=[]
-                    )
-                )
-            elif (dalAstNode["command"] == "set"):
-                print(dalAstNode["command"])
-                name = dalAstNode["args"][0]["value"]
-                value = dalAstNode["args"][2]["value"]
-                return ast.Assign(
-                    targets=[
-                        ast.Name(id=name, ctx=ast.Store())
-                    ],
-                    value=ast.Constant(value=value)
-                )
-
-
-    
---
+++ synthesizer/getSynthesizedNode.py
@@ -0,0 +1,60 @@
+import ast
+
+def getSynthesizedNode(dalAstNode):
+    '''
+        Get the AST node given the dalAST metadata.
+    '''
+    if (dalAstNode["type"] == "behavior"):
+        return ast.FunctionDef(
+            name=dalAstNode["behaviorName"],
+            args=ast.arguments(
+                posonlyargs=[],
+                args=[
+                    ast.arg(arg="worldState")
+                ],
+                kwonlyargs=[],
+                kw_defaults=[],
+                defaults=[],
+                vararg=None,
+                kwarg=None
+            ),
+            body=[],
+            decorator_list=[]
+        )
+
+    elif (dalAstNode["type"] == "design"):
+        return ast.Assign(
+            targets=[
+                ast.Name(id="design", ctx=ast.Store())
+            ],
+            value=ast.Constant(value=dalAstNode["design_name"][0]["value"])
+        )
+    elif (dalAstNode["type"] == "while"):
+        return ast.While(
+            test=ast.Name(id=dalAstNode["args"][0]["value"], ctx=ast.Load()),
+            body=[],
+            orelse=[]
+        )
+    elif (dalAstNode["type"] == "cmd"):
+        if (dalAstNode["command"] == "getInput"):
+            name = dalAstNode["args"][0]["value"]
+            return ast.Assign(
+                targets=[
+                    ast.Name(id=name, ctx=ast.Store())
+                ],
+                value=ast.Call(
+                    func=ast.Name(id="input", ctx=ast.Load()),
+                    args=[],
+                    keywords=[]
+                )
+            )
+        elif (dalAstNode["command"] == "set"):
+            print(dalAstNode["command"])
+            name = dalAstNode["args"][0]["value"]
+            value = dalAstNode["args"][2]["value"]
+            return ast.Assign(
+                targets=[
+                    ast.Name(id=name, ctx=ast.Store())
+                ],
+                value=ast.Constant(value=value)
+            )
