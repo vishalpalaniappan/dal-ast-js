431805b14293a54ebc906862dca155b202279683
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-07-22 09:58:58 -0400

    Move node ast to its own functions.

--- synthesizer/getSynthesizedNode.py
+++ synthesizer/getSynthesizedNode.py
@@ -6,62 +6,89 @@ def getSynthesizedNode(dalAstNode):
         Get the AST node given the dalAST metadata.
     '''
     if (dalAstNode["type"] == "behavior"):
-        return ast.FunctionDef(
-            name=dalAstNode["behaviorName"],
-            args=ast.arguments(
-                posonlyargs=[],
-                args=[],
-                kwonlyargs=[],
-                kw_defaults=[],
-                defaults=[],
-                vararg=None,
-                kwarg=None
-            ),
-            body=[
-                ast.Global(
-                    names=["worldState"]
-                )
-            ],
-            decorator_list=[]
-        )
-
+        return getBehaviorAst(dalAstNode)
     elif (dalAstNode["type"] == "design"):
-        return ast.Assign(
-            targets=[
-                ast.Name(id="design", ctx=ast.Store())
-            ],
-            value=ast.Constant(value=dalAstNode["design_name"][0]["value"])
-        )
-
+        return getDesignAst(dalAstNode)
     elif (dalAstNode["type"] == "while"):
-        return ast.While(
-            test=ast.Name(id=dalAstNode["args"][0]["value"], ctx=ast.Load()),
-            body=[],
-            orelse=[]
-        )
-
+        return getWhileAst(dalAstNode)
     elif (dalAstNode["type"] == "cmd"):
         if (dalAstNode["command"] == "getInput"):
-            name = dalAstNode["args"][0]["value"]
-            return ast.Assign(
-                targets=[
-                    ast.Name(id=name, ctx=ast.Store())
-                ],
-                value=ast.Call(
-                    func=ast.Name(id="input", ctx=ast.Load()),
-                    args=[],
-                    keywords=[]
-                )
-            )
-
+            return getCmdGetInputAst(dalAstNode)
         elif (dalAstNode["command"] == "set"):
-            name = dalAstNode["args"][0]["value"]
-            keys = dalAstNode["args"][1]["value"]
-            value = dalAstNode["args"][2]["value"]
-            print(name, keys, value)
-            return ast.Assign(
-                targets=[
-                    getVariableNameWithKeys(name, keys)
-                ],
-                value=ast.Constant(value=value)
+            return getCmdSetAst(dalAstNode)
+
+def getBehaviorAst(node):
+    '''
+        def <behaviorName>():
+            global worldState
+    '''
+    return ast.FunctionDef(
+        name=node["behaviorName"],
+        args=ast.arguments(
+            posonlyargs=[],
+            args=[],
+            kwonlyargs=[],
+            kw_defaults=[],
+            defaults=[],
+            vararg=None,
+            kwarg=None
+        ),
+        body=[
+            ast.Global(
+                names=["worldState"]
             )
+        ],
+        decorator_list=[]
+    )
+
+def getDesignAst(node):
+    '''
+        design = <design_Name>
+    '''
+    return ast.Assign(
+        targets=[
+            ast.Name(id="design", ctx=ast.Store())
+        ],
+        value=ast.Constant(value=node["design_name"][0]["value"])
+    )
+
+def getWhileAst(node):
+    '''
+        while <condition>:
+            <body>
+    '''
+    return ast.While(
+        test=ast.Name(id=node["args"][0]["value"], ctx=ast.Load()),
+        body=[],
+        orelse=[]
+    )
+
+def getCmdGetInputAst(node):
+    '''
+        <output> = input()
+    '''
+    name = node["args"][0]["value"]
+    return  ast.Assign(
+        targets=[
+            ast.Name(id=name, ctx=ast.Store())
+        ],
+        value=ast.Call(
+            func=ast.Name(id="input", ctx=ast.Load()),
+            args=[],
+            keywords=[]
+        )
+    )
+
+def getCmdSetAst(node):
+    '''
+        <name>[<keys>] = <value>
+    '''
+    name = node["args"][0]["value"]
+    keys = node["args"][1]["value"]
+    value = node["args"][2]["value"]
+    return ast.Assign(
+        targets=[
+            getVariableNameWithKeys(name, keys)
+        ],
+        value=ast.Constant(value=value)
+    )
