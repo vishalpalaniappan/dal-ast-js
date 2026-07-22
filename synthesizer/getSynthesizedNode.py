4ef548652d6c773ce22e43e03fae25c04467c4b5
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-07-22 10:18:09 -0400

    Add support for insert command.

--- synthesizer/getSynthesizedNode.py
+++ synthesizer/getSynthesizedNode.py
@@ -67,6 +67,10 @@ def getWhileAst(node):
 
 def getCmdGetInputAst(node):
     '''
+        This is a registered synthesis. I am claiming
+        that input() realises the meaning of the behavior
+        defined by getInput()
+
         <output> = input()
     '''
     name = node["args"][0]["value"]
@@ -93,4 +97,29 @@ def getCmdSetAst(node):
             getVariableNameWithKeys(name, keys)
         ],
         value=ast.Constant(value=value)
+    )
+
+def getCmdInsertAst(node):
+    '''
+        Command: insert(<target>, <keys>,  <value>, <index>)
+
+        Synthesized: <target>[<keys>].insert(<index>, <value>)
+    '''
+    target = node["args"][0]["value"]
+    keys = node["args"][1]["value"]
+    value = node["args"][2]["value"]
+    index = node["args"][3]["value"]
+    return ast.Expr(
+        value=ast.Call(
+            func=ast.Attribute(
+                value= getVariableNameWithKeys(target, keys),
+                attr="insert",
+                ctx=ast.Load()
+            ),
+            args=[
+                ast.Constant(index),
+                ast.Name(id=value, ctx=ast.Load()),
+            ],
+            keywords=[]
+        )
     )
