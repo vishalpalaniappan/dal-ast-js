a6f3987b5b1570f542453ed7c21db018c306ba64
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-07-22 20:50:39 -0400

    Cleanup comment.

--- src/DesignValidator.js
+++ src/DesignValidator.js
@@ -1,4 +1,5 @@
 // This will evaluate the semantic world for inconsistencies.
 // Example: Type mistmatch, invalid participants etc. 
 
-// This requires knowledge about the actual commands intended structure, so I will establish that before moving forward with the validation. The synthesis development can happen in parallel because it doesn't impact this process.
+// This requires knowledge about the actual transformations semantics, so I will establish that before moving forward with the validation. 
+// The synthesis development can happen in parallel because it doesn't impact this process.
