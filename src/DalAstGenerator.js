ff14aabe9960f914a4147ecb2c80eb2702db6335
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-07-24 10:15:01 -0400

    Add class to generate the AST (will export as library).

---
+++ src/DalAstGenerator.js
@@ -0,0 +1,34 @@
+import { DalLexer } from "./Lexer";
+import { DalParser } from "./Parser";
+/**
+ * This class produces an AST output given a DAL source file.
+ * 
+ * Internally it invokes:
+ * - Lexer
+ * - Parser
+ * - AST output
+ * 
+ * The AST output is sent to the synthesizer and synthesized
+ * into a python program.
+ * 
+ * In the future, this will also:
+ * - Identify syntax errors
+ * - Lint
+ * - Validate the internal consistency of the design
+ */
+export class DalAstGenerator {
+
+    constructor() {
+        this.ast = null;
+    }
+
+    run () {
+        try {
+            const lexer = new DalLexer(source.toString());
+            const parser = new DalParser(lexer.scannedTokens);
+            this.ast = parser.ast;
+        } catch(e) {
+            console.error("Error generating the AST:", e);
+        }
+    }
+}
