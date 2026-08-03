04a81129cb1da4fd1f91c9da7b7a07666e317bba
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-08-02 21:39:30 -0400

    Add tests script and test design.

--- tests/AST.test.js
+++ tests/AST.test.js
@@ -1,15 +1,14 @@
 import {describe, expect, it} from "vitest";
 import {resolve} from "path"
 import {readFile, unlink, writeFile} from "fs/promises"
-import { DesignValidator } from "../src/DesignValidator";
 import { DalParser } from "../src/Parser";
 import { DalLexer} from "../src/Lexer";
 import { DalAstGenerator } from "../src/DalAstGenerator";
 
 
-describe("Lexer", () => {
+describe("tests ast genertor from dal file", () => {
     it("basic source", async () => {
-        const filePath = resolve(__dirname, "./designs/library_manager.dal")
+        const filePath = resolve(__dirname, "./designs/reverse_name_persist.dal")
         const source = await readFile(filePath)
         const lexer = new DalLexer(source.toString());
 
@@ -26,16 +25,24 @@ describe("Lexer", () => {
             ast_output_path,
             JSON.stringify(parser.ast, null, 4)
         );
-
-        const validator = new DesignValidator(parser.ast);
-        validator.run();
     });
 
     it("tests direct ast generation", async () => {
-        const filePath = resolve(__dirname, "./designs/library_manager.dal")
+        const filePath = resolve(__dirname, "./designs/reverse_name_persist.dal")
         const source = await readFile(filePath)
         const ast = new DalAstGenerator().run(source.toString());
         const ast_output_path = resolve(__dirname, "./output/ast_direct_gen.json")
+        await writeFile(
+            ast_output_path,
+            JSON.stringify(ast, null, 4)
+        );
+    });
+
+    it("tests actor ast generation", async () => {
+        const filePath = resolve(__dirname, "./designs/actorTest.dal")
+        const source = await readFile(filePath)
+        const ast = new DalAstGenerator().run(source.toString());
+        const ast_output_path = resolve(__dirname, "./output/ast_actor.json")
         await writeFile(
             ast_output_path,
             JSON.stringify(ast, null, 4)
